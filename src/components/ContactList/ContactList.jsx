ba126535e3fa758653bf841673e41b7dcb443946
eduard-kaluhin <renoserviser@gmail.com>
2023-08-05 15:35:05 +0300

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import { List } from './ContactList.styled';
 import ContactItem from './ContactItem';
 
-const ContactList = ({ contacts, filter, onDeleteContact }) => {
+const ContactList = ({ contacts, onDeleteContact }) => {
   return (
     <List>
       {contacts
